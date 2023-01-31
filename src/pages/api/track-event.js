c35011aec898f4a692006bbc7a4f7c228f8d657b
AustinSpinazze <aspinazze94@gmail.com>
2023-01-31 16:36:03 -0600

    Added ipinfo package

--- src/pages/api/track-event.js
+++ src/pages/api/track-event.js
@@ -1,7 +1,18 @@
+const { IPinfoWrapper, LruCache } = require('node-ipinfo');
+
+const cacheOptions = {
+  max: 5000,
+  maxAge: 24 * 1000 * 60 * 60,
+};
+const cache = new LruCache(cacheOptions);
+const ipinfo = new IPinfoWrapper('MY_TOKEN', cache);
+
 export default async function handler(req, res) {
   let ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
 
-  console.log(ip);
+  ipinfoWrapper.lookupIp(`${ip}`).then((response) => {
+    console.log(response);
+  });
 
   res.status(201).send({ message: 'Success.' });
 }
