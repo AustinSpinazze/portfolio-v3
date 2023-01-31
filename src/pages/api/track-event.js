6eed8642c0a73ee74c5573e59569e090f41db845
AustinSpinazze <aspinazze94@gmail.com>
2023-01-31 16:40:29 -0600

    Updated with correct env and fixed type error

--- src/pages/api/track-event.js
+++ src/pages/api/track-event.js
@@ -5,12 +5,12 @@ const cacheOptions = {
   maxAge: 24 * 1000 * 60 * 60,
 };
 const cache = new LruCache(cacheOptions);
-const ipinfo = new IPinfoWrapper('MY_TOKEN', cache);
+const ipinfo = new IPinfoWrapper(process.env.IP_INFO_TOKEN, cache);
 
 export default async function handler(req, res) {
   let ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
 
-  ipinfoWrapper.lookupIp(`${ip}`).then((response) => {
+  ipinfo.lookupIp(`${ip}`).then((response) => {
     console.log(response);
   });
 
