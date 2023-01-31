f9308f51b365f31cbe08ad9a237e09386ab51918
AustinSpinazze <aspinazze94@gmail.com>
2023-01-31 16:03:20 -0600

    Updated track event logging

--- src/pages/api/track-event.js
+++ src/pages/api/track-event.js
@@ -1,9 +1,7 @@
 import { NextRequest } from 'next/server';
 
 export default async function handler(req, res) {
-  if (req.ip) {
-    console.log('ip: ', req.ip);
-  }
+  console.log('ip: ', req.ip);
 
   res.status(201).send({ message: 'Success.' });
 }
